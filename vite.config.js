4adc2e7f690ed373dd0b7273441aa937f9ca9197
rodvyx <yeshurion@gmail.com>
2025-10-06 21:52:59 +0300

    Deploying the Frontend

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blog-frontend-react/", // 👈 EXACT repo name
+  base: "/blog-frontend-react/", // 👈 important: must match your repo name
 });
-
 
